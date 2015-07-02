50e5266d46642a3a7cccb75dbfe8ba53eb60d167
yzhuo <yzhuo@uncc.edu>
2015-07-02 14:18:29 -0400

    redesign main menu

--- PhoneApp/app/src/main/java/com/yate/phoneapp/MainActivity.java
+++ PhoneApp/app/src/main/java/com/yate/phoneapp/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
         ButterKnife.inject(this); //gives access to views defined above
 
         setSupportActionBar(toolbar);
-        addItemsOnSpinner();
+        //addItemsOnSpinner();
         //gives us buttons and lets us set action bar
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.title, R.string.title);
         //Attaching the drawerToggle to the drawerLayout
@@ -54,16 +54,8 @@ public class MainActivity extends ActionBarActivity {
         drawerRecyclerView.setAdapter(drawerAdapter);
         drawerRecyclerView.setHasFixedSize(true);
         drawerRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    public void addItemsOnSpinner() {
         Spinner spinner = (Spinner) findViewById(R.id.options_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -73,4 +65,26 @@ public class MainActivity extends ActionBarActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /*
+    public void addItemsOnSpinner(Bundle savedInstanceState) {
+        //super.addItemsOnSpinner(savedInstanceState);
+        Spinner spinner = (Spinner) findViewById(R.id.options_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.App_Options, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+    */
+
 }
