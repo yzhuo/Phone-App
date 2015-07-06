2beb8e4acf924653202e28f9591dfde8f471b3d1
yzhuo <yzhuo@uncc.edu>
2015-07-06 13:25:31 -0400

    Revert "fix sync issues"
    This reverts commit 61da5f9ea72a02e50863a45f58aca6f2935c1e4f.

--- PhoneApp/app/src/main/java/com/yate/phoneapp/DrawerAdapter.java
+++ PhoneApp/app/src/main/java/com/yate/phoneapp/DrawerAdapter.java
@@ -58,9 +58,11 @@ public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder
                     break;
                 case "Favorites": holder.imageView.setImageResource(R.drawable.ic_favorite);
                     break;
+                case "Save": holder.imageView.setImageResource(R.drawable.ic_save);
+                    break;
                 case "Map": holder.imageView.setImageResource(R.drawable.ic_map);
                     break;
-                case "Saved Pages": holder.imageView.setImageResource(R.drawable.ic_save);
+                case "Search": holder.imageView.setImageResource(R.drawable.ic_action_search);
                     break;
                 case "About": holder.imageView.setImageResource(R.drawable.ic_about);
                     break;
--- PhoneApp/app/src/main/java/com/yate/phoneapp/MainActivity.java
+++ PhoneApp/app/src/main/java/com/yate/phoneapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.yate.phoneapp;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -64,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         rows.add("Home");
         rows.add("Favorites");
         rows.add("Map");
+        rows.add("Search");
         rows.add("About");
         rows.add("Saved Pages");
 
