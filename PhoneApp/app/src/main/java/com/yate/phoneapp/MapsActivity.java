51c0b5ec49890bad25d64251cced148229aa0d35
yzhuo <yzhuo@uncc.edu>
2015-07-12 16:36:23 -0400

    add top tool bar for map page

--- PhoneApp/app/src/main/java/com/yate/phoneapp/MapsActivity.java
+++ PhoneApp/app/src/main/java/com/yate/phoneapp/MapsActivity.java
@@ -1,11 +1,19 @@
 package com.yate.phoneapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,7 +35,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         //check if google map is available
         mapFragment.getMapAsync(this);
-        
+
+        //******************text activity
+        EditText editText = (EditText) findViewById(R.id.search);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Button send = (Button) findViewById(R.id.send);
+                }
+                return handled;
+            }
+        });
+        //****************text activity end
+
+        //***********able to hide keyboard when outside the text
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+        //******hide keyboard end***
+
+        //**************reset activity
+        Button reset = (Button)findViewById(R.id.clear);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText search = (EditText) findViewById(R.id.search);
+                search.setText("");
+            }
+        });
+        //************reset activity end
     }
 
     @Override
@@ -123,4 +166,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
+
+    //*************hide keyboard ******************
+    public void hideKeyboard(View view){
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+    //**************end hide keybaord
 }
